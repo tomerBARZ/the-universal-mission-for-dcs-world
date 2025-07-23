a5c6f4f75399965ba209b082630ce88006ef1407
Ambroise Garel <47314805+akaAgar@users.noreply.github.com>
2025-07-23 12:01:14 +0200

    Added wingman spawning logic

--- Script/The Universal Mission/SupportWingmen.lua
+++ Script/The Universal Mission/SupportWingmen.lua
@@ -12,6 +12,31 @@ do
         if TUM.settings.getValue(TUM.settings.id.MULTIPLAYER) then return end -- No wingmen in multiplayer
         TUM.supportWingmen.removeAll() -- Destroy all pre-existing wingmen
         TUM.log("Creating wingmen...")
+
+        local player = world:getPlayer()
+        if not player then return end
+
+        local count = 1 -- TODO
+
+        local groupInfo = DCSEx.unitGroupMaker.create(
+            TUM.settings.getPlayerCoalition(),
+            Group.Category.AIRPLANE, -- TODO: or helicopter!
+            DCSEx.math.randomPointInCircle(DCSEx.math.vec3ToVec2(player:getPoint()), 500, 250),
+            { player:getTypeName() },
+            {
+                silenced = true,
+                taskFollow = DCSEx.dcs.getObjectIDAsNumber(player:getGroup()),
+                unlimitedFuel = true
+            }
+        )
+
+        if not groupInfo then
+            TUM.log("Failed to spawn AI wingmen", TUM.logLevel.WARNING)
+            return
+        end
+
+        TUM.log("Spawned AI wingmen")
+        wingmenGroupID = groupInfo.groupID
     end
 
     function TUM.supportWingmen.removeAll()
